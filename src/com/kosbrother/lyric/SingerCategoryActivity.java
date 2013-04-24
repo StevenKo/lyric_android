b88ee7624fe43ff4c7a94c4aa833c3e682263592
Steven Ko <chunyuko85@gmaill.com>
2013-04-24 20:01:02 +0800

    singercategory first is Chuin

--- src/com/kosbrother/lyric/SingerCategoryActivity.java
+++ src/com/kosbrother/lyric/SingerCategoryActivity.java
@@ -16,84 +16,85 @@ import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.kosbrother.fragment.HotSongFragment;
 import com.kosbrother.fragment.SingerCategoryFragment;
 import com.kosbrother.lyric.api.LyricAPI;
 import com.kosbrother.lyric.entity.SingerSearchWay;
 import com.viewpagerindicator.TabPageIndicator;
 
 @SuppressLint("NewApi")
-public class SingerCategoryActivity extends FragmentActivity{
-	
-	private ViewPager pager;
-	private ArrayList<SingerSearchWay> mCategory;
-	private Bundle mBundle;
-	private int categoryId;
-	private String categoryName;
-	private AlertDialog.Builder aboutUsDialog;
-	
-	@Override
+public class SingerCategoryActivity extends FragmentActivity {
+
+    private ViewPager                  pager;
+    private ArrayList<SingerSearchWay> mCategory;
+    private Bundle                     mBundle;
+    private int                        categoryId;
+    private String                     categoryName;
+    private AlertDialog.Builder        aboutUsDialog;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simple_tabs);
-        
+
         mBundle = this.getIntent().getExtras();
         categoryId = mBundle.getInt("SingerCategoryId");
         categoryName = mBundle.getString("SingerCategoryName");
-        
+
         setTitle(categoryName);
-        int sdkVersion = android.os.Build.VERSION.SDK_INT; 
-        if(sdkVersion > 10){
-        	getActionBar().setDisplayHomeAsUpEnabled(true);
+        int sdkVersion = android.os.Build.VERSION.SDK_INT;
+        if (sdkVersion > 10) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        
-        mCategory = LyricAPI.getSingerCategoryWays(categoryId);;
-        
+
+        mCategory = LyricAPI.getSingerCategoryWays(categoryId);
+        ;
+
         FragmentStatePagerAdapter adapter = new GoogleMusicAdapter(getSupportFragmentManager());
 
-        pager = (ViewPager)findViewById(R.id.pager);
+        pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(adapter);
 
-        TabPageIndicator indicator = (TabPageIndicator)findViewById(R.id.indicator);
+        TabPageIndicator indicator = (TabPageIndicator) findViewById(R.id.indicator);
         indicator.setViewPager(pager);
-        
+        indicator.setCurrentItem(1);
+
         setAboutUsDialog();
     }
-	
-	
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-    }
-	
-	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 
-	    int itemId = item.getItemId();
-	    switch (itemId) {
-	    case android.R.id.home:
-	        finish();
-	        break;
-	    case R.id.action_about:
-	    	aboutUsDialog.show();
-	        break;
-	    case R.id.action_contact:
-	    	final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-	    	emailIntent.setType("plain/text");
-	    	emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"brotherkos@gmail.com"});
-	    	emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "聯絡我們 from 歌曲王國");
-	    	emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
-	    	startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-	        break;
-	    case R.id.action_grade:
-	    	Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.recommend_url)));
-			startActivity(browserIntent);
-	        break;
-	    }
-	    return true;
-	}
-	
-	class GoogleMusicAdapter extends FragmentStatePagerAdapter {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+
+        int itemId = item.getItemId();
+        switch (itemId) {
+        case android.R.id.home:
+            finish();
+            break;
+        case R.id.action_about:
+            aboutUsDialog.show();
+            break;
+        case R.id.action_contact:
+            final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+            emailIntent.setType("plain/text");
+            emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { "brotherkos@gmail.com" });
+            emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "聯絡我們 from 歌曲王國");
+            emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            break;
+        case R.id.action_grade:
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.recommend_url)));
+            startActivity(browserIntent);
+            break;
+        }
+        return true;
+    }
+
+    class GoogleMusicAdapter extends FragmentStatePagerAdapter {
         public GoogleMusicAdapter(FragmentManager fm) {
             super(fm);
         }
@@ -101,10 +102,9 @@ public class SingerCategoryActivity extends FragmentActivity{
         @Override
         public Fragment getItem(int position) {
 
-        	SingerCategoryFragment kk = new SingerCategoryFragment(mCategory.get(position).getId());
+            SingerCategoryFragment kk = new SingerCategoryFragment(mCategory.get(position).getId());
             return kk;
         }
-       
 
         @Override
         public CharSequence getPageTitle(int position) {
@@ -116,16 +116,16 @@ public class SingerCategoryActivity extends FragmentActivity{
             return mCategory.size();
         }
     }
-	
-	 private void setAboutUsDialog() {
-	        // TODO Auto-generated method stub
-	        aboutUsDialog = new AlertDialog.Builder(this).setTitle(getResources().getString(R.string.about_us_string)).setIcon(R.drawable.app_icon_72)
-	                .setMessage(getResources().getString(R.string.about_us))
-	                .setPositiveButton(getResources().getString(R.string.yes_string), new DialogInterface.OnClickListener() {
-	                    @Override
-	                    public void onClick(DialogInterface dialog, int which) {
 
-	                    }
-	                });
-	    }
+    private void setAboutUsDialog() {
+        // TODO Auto-generated method stub
+        aboutUsDialog = new AlertDialog.Builder(this).setTitle(getResources().getString(R.string.about_us_string)).setIcon(R.drawable.app_icon_72)
+                .setMessage(getResources().getString(R.string.about_us))
+                .setPositiveButton(getResources().getString(R.string.yes_string), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+    }
 }
